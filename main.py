c393824e94d1a5dadac3e1a285ca30393d027df8
RomanB <djimbik93@gmail.com>
2024-03-27 07:38:21 +0300

    new task 8 kyu

---
+++ main.py
@@ -0,0 +1,6 @@
+# 8kyu #############################################################################
+# https://www.codewars.com/kata/53f0f358b9cb376eca001079/solutions/python
+class Ball(object):
+    def __init__(self, type="regular"):
+        self.ball_type = type
+
