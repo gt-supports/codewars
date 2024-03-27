abc55594566a0f93379f9721270c9199a7c3633f
RomanB <djimbik93@gmail.com>
2024-03-27 13:29:00 +0300

    my commit

--- 8kyu/main.py
+++ 8kyu/main.py
@@ -1,7 +1,4 @@
-# 8kyu #############################################################################
 # https://www.codewars.com/kata/53f0f358b9cb376eca001079/solutions/python
 class Ball(object):
     def __init__(self, type="regular"):
         self.ball_type = type
-
-# test comment
