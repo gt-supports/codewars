bc04d7e0abbd3c89157ec7865d014849812a5b5c
RomanB <djimbik93@gmail.com>
2024-04-01 07:26:05 +0300

    8kyu add commit

--- 8kyu/main.py
+++ 8kyu/main.py
@@ -2,3 +2,13 @@
 class Ball(object):
     def __init__(self, type="regular"):
         self.ball_type = type
+
+
+def middle_me(N, X, Y):
+    # your code here
+    if N % 2 != 0:
+        return X
+    return f'{Y * (N / 2)}{X}{Y * (N / 2)}'
+
+
+middle_me(18, 'z', '#')
