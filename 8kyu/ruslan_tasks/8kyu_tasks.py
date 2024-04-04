e2b4e9fb8138b5824d48c63b342f20ba1dbedc75
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-04 07:50:01 +0600

    haven't finished yet

--- 8kyu/ruslan_tasks/8kyu_tasks.py
+++ 8kyu/ruslan_tasks/8kyu_tasks.py
@@ -14,5 +14,16 @@ def basic_op(operator, value1, value2):
 def remove_exclamation_marks(s):
     return s.replace('!', '')
 
+# https://www.codewars.com/kata/5861d28f124b35723e00005e/solutions/python
 def zero_fuel(distance_to_pump, mpg, fuel_left):
     return mpg * fuel_left >= distance_to_pump
+
+# https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/python
+def reverse_words(text):
+    print( ' '.join([''.join(list(words)[::-1]) for words in text.split()]))
+    print(''.join([words for words in list(text) if words.isalpha()]))
+    print(' '.join([''.join(list(words)[::-1]) for words in text.split()]))
+    print(' '.join([''.join(list(words)[::-1]) for words in text.split()]))
+    return ' '.join([''.join(list(words)[::-1]) for words in text.split()])
+# haven't finished yet
+reverse_words("double  spaced")
