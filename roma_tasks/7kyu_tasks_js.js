3a00f2aef23765199dc80917a60ef411db50829f
RomanB <djimbik93@gmail.com>
2024-04-04 10:53:39 +0300

    second task - js

--- roma_tasks/7kyu_tasks_js.js
+++ roma_tasks/7kyu_tasks_js.js
@@ -6,3 +6,11 @@ const isSquare = function (arr) {
     }
     return arr.filter(num => Number.isInteger(Math.sqrt(num))).length == arr.length
 }
+
+
+// https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/solutions/javascript?filter=me&sort=best_practice&invalids=false
+function rowWeights(array) {
+    const team1 = array.reduce((sum, value, i) => i % 2 === 0 ? sum + value : sum, 0)
+    const team2 = array.reduce((sum, value, i) => i % 2 !== 0 ? sum + value : sum, 0)
+    return [team1, team2]
+}
