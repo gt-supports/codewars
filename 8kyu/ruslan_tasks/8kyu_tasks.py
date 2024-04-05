207ad8e6ead386f8cfb70f4c257d1229e8e614b3
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-05 09:10:12 +0600

    two

--- 8kyu/ruslan_tasks/8kyu_tasks.py
+++ 8kyu/ruslan_tasks/8kyu_tasks.py
@@ -32,3 +32,6 @@ reverse_words("double  spaced")
 def set_alarm(employed, vacation):
     return employed and not vacation
 
+# https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/python
+def greet(name, owner):
+    return "Hello boss" if name == owner else "Hello guest"
