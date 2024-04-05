891c66f60402ab3d4eab6e1068e900acdd415b8c
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-05 09:05:10 +0600

    one

--- 8kyu/ruslan_tasks/8kyu_tasks.py
+++ 8kyu/ruslan_tasks/8kyu_tasks.py
@@ -27,3 +27,8 @@ def reverse_words(text):
     return ' '.join([''.join(list(words)[::-1]) for words in text.split()])
 # haven't finished yet
 reverse_words("double  spaced")
+
+# https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/python
+def set_alarm(employed, vacation):
+    return employed and not vacation
+
