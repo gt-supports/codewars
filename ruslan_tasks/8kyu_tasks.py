9dfc75036ecbbc74298660f43ccc7f323acedd18
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-11 18:00:01 +0600

    2024-04-11 one

--- ruslan_tasks/8kyu_tasks.py
+++ ruslan_tasks/8kyu_tasks.py
@@ -92,3 +92,7 @@ def switch_it_up(number):
     return d[number]
 
 switch_it_up(2)
+
+# https://www.codewars.com/kata/555086d53eac039a2a000083/train/python
+def lovefunc( flower1, flower2 ):
+    return (flower1 + flower2) % 2 == 1
