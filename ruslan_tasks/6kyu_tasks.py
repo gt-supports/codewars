39dd2823b33dd58254b522a55ec23ea8fe53dbc5
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-11 18:00:27 +0600

    2024-04-11 two

--- ruslan_tasks/6kyu_tasks.py
+++ ruslan_tasks/6kyu_tasks.py
@@ -145,7 +145,9 @@ def wave(people):
     print(my_wave)
     return my_wave
 
+# https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+def solution(s):
+    return ''.join([" " + letter  if letter.isupper() else letter for letter in list(s) ])
 
 
-
-wave("two words")
+solution("breakCamelCase")
