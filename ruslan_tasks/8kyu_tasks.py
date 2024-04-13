3fc91a463374be687b74c579c764f22690632afb
Ruslan Abdrakhimov <ruslanabdrakhimov@MacBook-Pro-Ruslan.local>
2024-04-13 08:41:44 +0600

    13-02-2024 one

--- ruslan_tasks/8kyu_tasks.py
+++ ruslan_tasks/8kyu_tasks.py
@@ -113,3 +113,7 @@ def minimum(arr):
 
 def maximum(arr):
     return max(arr)
+
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
