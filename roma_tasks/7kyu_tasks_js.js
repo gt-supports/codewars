1c22704e67d0e9ead8f1bec3b8768e8ecd97508c
RomanB <djimbik93@gmail.com>
2024-04-29 08:52:56 +0300

    1 task - js - 7kyu

--- roma_tasks/7kyu_tasks_js.js
+++ roma_tasks/7kyu_tasks_js.js
@@ -91,3 +91,13 @@ const greet = function (name) {
     const newName = name.toLowerCase()
     return `Hello ${newName[0].toUpperCase() + newName.slice(1)}!`
 };
+
+//https://www.codewars.com/kata/59a8570b570190d313000037/train/javascript
+
+function sumCubes(n) {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+        sum += i ** 3
+    }
+    return sum
+}
