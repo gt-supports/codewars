b02347b34ff44d3650ed2cb0ca90a729aad7bb4f
qarlow <gipsyteamigor@gmail.com>
2024-04-30 19:32:21 +0300

    training/second dict

--- igor_tasks/8kyu_tasks.py
+++ igor_tasks/8kyu_tasks.py
@@ -1,1 +1,16 @@
 # test comments
+# https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/python
+
+def switch_it_up(number):
+    words = {
+        0: "Zero",
+        1: "One",
+        2: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    }
+    return words.get(number)
