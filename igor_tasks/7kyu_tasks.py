40d9ef0c7039d94b4d1145d9884967be05c6b5ef
qarlow <gipsyteamigor@gmail.com>
2024-05-05 23:21:40 +0300

    list_task

--- igor_tasks/7kyu_tasks.py
+++ igor_tasks/7kyu_tasks.py
@@ -43,3 +43,13 @@ def reverse_words(text):
 
 def find_short(s):
     return len(min(s.split(), key=len))
+
+
+# https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
+
+def filter_list(l):
+    new_list = []
+    for i in l:
+        if type(i) is int:
+            new_list.append(i)
+    return new_list
--- igor_tasks/8kyu_tasks.py
+++ igor_tasks/8kyu_tasks.py
@@ -26,3 +26,16 @@ def double_char(s):
 
 def say_hello(name):
     return f"Hello, {name}"
+
+# https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/python
+
+def how_much_i_love_you(nb_petals):
+    petals = {
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly",
+        0: "not at all"
+    }
+    return petals[nb_petals % 6]
