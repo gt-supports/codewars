64be7f17690b1bbcde8a50ff33105540d00ae918
RomanB <djimbik93@gmail.com>
2024-05-11 19:18:01 +0300

    2 tasks js + python

--- roma_tasks/6kyu_tasks_python.py
+++ roma_tasks/6kyu_tasks_python.py
@@ -267,3 +267,10 @@ def find_it(seq):
     for i in seq:
         if (seq.count(i) % 2 != 0):
             return i
+
+
+# https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/python
+def is_pangram(st):
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    s = set(st.lower())
+    return alphabet <= s
