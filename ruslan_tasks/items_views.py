45d943bb17cb64bea4f5e602d6c174b310beb0a2
Ruslan <abdrakhimovrus@gmail.com>
2024-05-13 14:31:25 +0600

    13-05-2024 fastapi day 3

--- ruslan_tasks/items_views.py
+++ ruslan_tasks/items_views.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Path
 
-router = APIRouter(prefix="/items")
+router = APIRouter(prefix="/items", tags=["Items"])
 
 @router.get("/")
 def list_items():
