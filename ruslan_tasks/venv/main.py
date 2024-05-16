8bfe7847cc2a02aa23d5050b529dbde9ce1928cc
Ruslan <abdrakhimovrus@gmail.com>
2024-05-16 08:51:46 +0600

    16-05-2024 fastapi day five

--- ruslan_tasks/venv/main.py
+++ ruslan_tasks/venv/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 import uvicorn
 
-from items_views import router as items_router
+from ruslan_tasks.venv.items_views import router as items_router
 from users.views import router as user_router
 
 
