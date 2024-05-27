8fdc2e16c87e9c77a06dadae8c51d26515ca9ab3
Ruslan <abdrakhimovrus@gmail.com>
2024-05-27 17:31:30 +0600

    [UPDATE] 27-05-2024 codewars

--- ruslan_tasks/7kyu_tasks.py
+++ ruslan_tasks/7kyu_tasks.py
@@ -324,3 +324,9 @@ def factorial(n):
     if n == 0:
         return 1
     return n * factorial(n-1)
+
+
+# https://www.codewars.com/kata/526c7363236867513f0005ca/train/python
+
+def is_leap_year(year):
+    return year % 400 == 0 or (year % 4 == 0 and year %100 != 0)
