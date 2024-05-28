680bc83a36a5bbbdac0eec41f2d21c246c5c20c9
Ruslan <abdrakhimovrus@gmail.com>
2024-05-28 13:19:32 +0600

    [UPDATE] 28-05-2024 codewars

--- ruslan_tasks/fastAPI_lessons/api_v1/products/views.py
+++ ruslan_tasks/fastAPI_lessons/api_v1/products/views.py
@@ -1,20 +1,30 @@
 from fastapi import APIRouter, HTTPException, status
+from sqlalchemy.ext.asyncio import AsynSession
+from core.models import db_helper
 from schemas import Product, ProductCreate
 from . import crud
 
 router = APIRouter(tags=["Products"])
 
-@router.get(path: "/", response_model=lisy[Product])
-async def get_products(session):
+@router.get(path: "/", response_model=list[Product])
+async def get_products(session: AsyncSession = Depends(db_helper.session_dependency),
+                       ):
     return await crud.get_products(session=session)
 
 @router.post(path: "/", response_model=Product)
-async def create_product(session, product_in: ProductCreate):
+async def create_product(
+                         product_in: ProductCreate,
+                         session:AsynSession = Depends(db_helper.session_dependency),
+                         ):
+
     return await crud.create_product(session=session, product_in=product_in)
 
 
 @router.get("/{product_id}/", response_model = Product)
-async def get_product(product_id:int, session):
+async def get_product(
+        product_id:int,
+        session: AsyncSession = Depends(db_helper.session_dependency),
+):
     product = await crud.get_products(session=session, product_id=product_id)
     if product is not None:
         return product
